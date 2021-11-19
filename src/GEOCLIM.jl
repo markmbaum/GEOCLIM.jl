e3d68b7e9f13dd7362f005146077e9b87019e7c8
Minmin Fu <minminjfu@gmail.com>
2021-11-19 14:38:30 -0500

    added MAC weathering scheme

--- src/GEOCLIM.jl
+++ src/GEOCLIM.jl
@@ -24,7 +24,7 @@ include("ClimatologyInterpolator.jl")
 #------------------------------------------------------------------------------
 # weathering functions
 
-export weathering, totalweathering
+export weathering, totalweathering, weathering_mac, totalweathering_mac
 
 #--------------------------------------
 # As implementated by Goddéris et al. 2017
@@ -49,29 +49,66 @@ end
 #--------------------------------------
 # As implementated by Abbot et al. 2012 
 # pCO2 dependence is added and the temperature dependence is slightly different
+const β  = 0.2
 
-function weathering(r, T, A, pCO2, k, Eₐ, T₀, pCO2₀, β) 
-    k*r*A*exp((Eₐ/𝐑)*(T - T₀)/T₀^2)*(pCO2/pCO2₀)^β
+function weathering(r, T, A, pCO2, k, Tₑ, T₀, pCO2₀) 
+    k*r*A*exp((T - T₀)/Tₑ)*(pCO2/pCO2₀)^β
 end
 
-function weathering(𝒸::Climatology, pCO2, k, Eₐ, T₀, pCO2₀, β)
-    weathering.(𝒸.r, 𝒸.T, 𝒸.A, pCO2, k, Eₐ, T₀, pCO2₀, β)
+function weathering(𝒸::Climatology, pCO2, k, Tₑ, T₀, pCO2₀)
+    weathering.(𝒸.r, 𝒸.T, 𝒸.A, pCO2, k, Tₑ, T₀, pCO2₀)
 end
 
-function totalweathering(𝒸::Climatology, pCO2, k, Eₐ, T₀, pCO2₀, β)
+function totalweathering(𝒸::Climatology, pCO2, k, Tₑ, T₀, pCO2₀)
     @unpack mask, r, T, A, n, m = 𝒸
     ΣW = 0.0
     @inbounds for i ∈ 1:n, j ∈ 1:m
         if mask[i,j]
-            ΣW += weathering(r[i,j], T[i,j], A[i,j], pCO2, k, Eₐ, T₀, pCO2₀, β)
+            ΣW += weathering(r[i,j], T[i,j], A[i,j], pCO2, k, Tₑ, T₀, pCO2₀)
         end
     end
     return ΣW
 end
 
 #------------------------------------------------------------------------------
-# As implementated by Graham and Pierrehumbert 2020 
+# MAC, as implementated by Graham and Pierrehumbert 2020 
 # following Maher and Chamberlin 2014
+
+const n = 0.316 # Thermodynamic pCO2 dependence [-]
+const Λ = 1.4e-3 # Thermodynamic coefficient for Ceq [-]
+const L = 1 # Flow path length [m] 
+const ϕ = 0.1 # Porosity [-]
+const ρ = 12728 # Mineral mass to fluid volume ratio [kg m⁻³]
+const k₀ = 8.7e-6 # Reference rate constant [mol m⁻² yr⁻¹]
+const 𝐀 = 100 # Specific surface area [m²kg⁻¹]
+const X = 0.36 # Reactive mineral conc. in fresh rock [-]
+const tₛ = 1e5 # Soil age [yr]
+const m = 0.27 # Mineral molar mass [kg/mol]
+const μ = ℯ^2 # Scaling constant [-]
+const α = L*ϕ*ρ*𝐀*X*μ # Defined for convenience [-]
+
+function Ceq(pCO2)
+    return Λ*(pCO2*1e-6)^n*1000 #conversion from mol/liter to mol/m3, ppm to bar
+end
+
+function weathering_mac(r, T, A, pCO2, Tₑ, T₀, pCO2₀) 
+    A*α*((k₀*exp((T - T₀)/Tₑ)*(pCO2/pCO2₀)^β)^-1 + m*𝐀*tₛ + α/(r*Ceq(pCO2)))^-1
+end
+
+function weathering_mac(𝒸::Climatology, pCO2, Tₑ, T₀, pCO2₀) 
+    weathering_mac.(𝒸.r, 𝒸.T, 𝒸.A, pCO2, Tₑ, T₀, pCO2₀)
+end
+
+function totalweathering_mac(𝒸::Climatology, pCO2, Tₑ, T₀, pCO2₀)
+    @unpack mask, r, T, A, n, m = 𝒸
+    ΣW = 0.0
+    @inbounds for i ∈ 1:n, j ∈ 1:m
+        if mask[i,j]
+            ΣW += weathering_mac(r[i,j], T[i,j], A[i,j], pCO2, Tₑ, T₀, pCO2₀)
+        end
+    end
+    return ΣW
+end
 
 
 #====
